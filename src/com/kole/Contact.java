772def484fc87cb9dfd1a90abbe76ad873b324ca
Kole <slobodan.kosanovic@gmail.com>
2018-05-27 19:02:32 +0200

    a

--- src/com/kole/Contact.java
+++ src/com/kole/Contact.java
@@ -1,5 +1,23 @@
 package com.kole;
 
 public class Contact {
+    private String name;
+    private int number;
 
+    public Contact(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Contact addContact(String name, int number){
+        return new Contact(name,number);
+    }
 }
--- src/com/kole/MobilePhone.java
+++ src/com/kole/MobilePhone.java
@@ -1,6 +1,31 @@
 package com.kole;
 
+import java.util.ArrayList;
+
 public class MobilePhone {
+    private String name;
+    private int number;
+    ArrayList<Contact> contacts;
 
-
+    public MobilePhone(String name, int number) {
+        this.name = name;
+        this.number = number;
+        this.contacts = new ArrayList<>();
     }
+
+    public void addContact(String name, int number) {
+        contacts.add(new Contact(name, number));
+    }
+
+    private Contact findContact(String name, int number) {
+        Contact checkedContact = new Contact(name, number);
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getName().contains(name)) {
+                System.out.println("Contact already exists wigga");
+                return null;
+            } else contacts.add(new Contact(name, number));
+
+        }
+        return checkedContact;
+    }
+}
