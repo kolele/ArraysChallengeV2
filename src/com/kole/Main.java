92d3661f52142770df60f807e2189f41e20f996a
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-05-28 16:06:50 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -6,5 +6,11 @@ public class Main{
         mobilePhone.addContact("Kole", 15975);
         mobilePhone.addContact("Sale", 9586);
         mobilePhone.printContacts();
+        mobilePhone.updateContact("Kole", 1111);
+        mobilePhone.printContacts();
+        mobilePhone.updateContact("Sale", 2222);
+        mobilePhone.printContacts();
+        mobilePhone.removeContact("Kole");
+        mobilePhone.printContacts();
     }
 }
